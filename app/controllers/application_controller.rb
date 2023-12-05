b3badef3b0242789c1af2af91a3c2b9ad7349b4c
EC2 Default User <ec2-user@ip-172-31-14-130.ap-northeast-1.compute.internal>
2023-12-05 11:50:30 +0000

    Usersのエラーを解消した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top, :about] # top, about の2つのアクションのみ、ログイン無しでもアクセス可能にする
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user) # ログインした直後は、ユーザーの詳細ページに遷移
+    user_path(current_user)
   end
 
   protected
